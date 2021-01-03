68b2d1c6267383c79077183488178198d04c2802
iris-lew <iris.lew@berkeley.edu>
2021-01-03 00:04:00 -0500

    Removed stop words

--- LinkedIn_Script.R
+++ LinkedIn_Script.R
@@ -7,7 +7,7 @@ getwd()
 ####Plan is to eventually use the RLinkedIn package,
 ####but it doesn't seem to be able to take the recommendations from the profile now.
 
-packages <- c("Rlinkedin")
+packages <- c("Rlinkedin","dplyr","tm", "tidytext","tau","tidyr")
 #Load and install the packages (if not already installed)
 packages.check <- lapply (
   packages,
@@ -19,4 +19,13 @@ packages.check <- lapply (
   } 
 )
 
-in.auth <- inOAuth()
+#in.auth <- inOAuth() ##Not necessary right now because not using Rlinkedin API yet...
+
+#Load in the raw data
+rawdata <- read_excel("Recs.xlsx")
+
+#list of common English stopwords
+stop <- tm::stopwords("english")
+
+#Remove the Stop Words
+removed <- remove_stopwords(rawdata[,1],stop, lines=TRUE)
