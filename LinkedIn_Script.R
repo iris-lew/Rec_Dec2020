4563f615a56ba3313e43eeeee71ec81b86717310
iris-lew <iris.lew@berkeley.edu>
2021-01-10 22:07:10 -0500

    Added the word cloud functions

--- LinkedIn_Script.R
+++ LinkedIn_Script.R
@@ -29,3 +29,33 @@ stop <- tm::stopwords("english")
 
 #Remove the Stop Words
 removed <- remove_stopwords(rawdata[,1],stop, lines=TRUE)
+
+# Create a corpus  
+docs <- Corpus(VectorSource(removed))
+
+docs <- docs %>%
+  tm_map(removeNumbers) %>%
+  tm_map(removePunctuation) %>%
+  tm_map(stripWhitespace)
+
+docs <- tm_map(docs, content_transformer(tolower))
+docs <- tm_map(docs, removeWords, stopwords("english"))
+
+#Taken from 
+###https://www.rdocumentation.org/packages/tm/versions/0.7-8/topics/Corpus
+
+#Create the Bag of Word Model
+dtm <- DocumentTermMatrix(docs)
+dtm <- removeSparseTerms(dtm, 0.999)
+dataset <- as.data.frame(as.matrix(dtm))
+dataset <- as.matrix(dtm)
+
+#Turn the Bag of Words into the count of the number of times words appear
+v <- sort(colSums(dataset),decreasing=TRUE)
+#This will get the  unique words in the Wordcloud
+myNames <- names(v)
+#Table of the words and the number of times they appear
+d <-  data.frame(word=myNames,freq=v)
+
+#Function to create the wordcloud
+wordcloud(d$word,scale=c(4,.3), colors=c(6,4),random.color=FALSE, d$freq, min.freq=80)
